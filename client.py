5148f395df60e859bb2befea4964753525fd24ec
julialoy <julialoy@gmail.com>
2022-11-30 12:20:07 -0500

    Using threading (tutorial via Real Python) to allow consecutive messages from client/server. Neither socket closes correctly.

--- client.py
+++ client.py
@@ -1,21 +1,43 @@
 from socket import *
+import threading
+import concurrent.futures
 
 server_host = 'localhost'
 server_port = 3291
 client_port = 3478
 quit_msg = "Type /q to quit"
 
+def server_thread_func(sckt, event):
+    while not event.is_set():
+        server_msg = sckt.recv(1024)
+        while len(server_msg) > 0:
+            print(f"SERVER MESSAGE LENGTH: {len(server_msg)}")
+            print(server_msg.decode())
+            server_msg = sckt.recv(1024)
+
+        event.set()
+
+def client_thread_func(sckt, event):
+    print(f"Type a message")
+    while not event.is_set():
+        client_msg = input('> ')
+        client_msg_len = len(client_msg)
+        if client_msg == "/q":
+            print(f"Closing connection")
+            # client_sock.close()
+            event.set()
+            return
+        # else:
+        client_data = str(client_msg_len) + ' ' + client_msg
+        sckt.send(client_data.encode())
+
+
 with socket(AF_INET, SOCK_STREAM) as client_sock:
     client_sock.connect((server_host, server_port))
-    while True:
-        client_msg = input('> ')
+    print(quit_msg)
+    # while client_msg != '/q':
+    event = threading.Event()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        executor.submit(client_thread_func, client_sock, event)
+        executor.submit(server_thread_func, client_sock, event)
 
-        if client_msg == "/q":
-            client_sock.close()
-        else:
-            client_sock.send(client_msg.encode())
-            server_msg = client_sock.recv(1024)
-            while len(server_msg) > 0:
-                print(server_msg)
-                client_sock.recv(1024)
-                
--- server.py
+++ server.py
@@ -3,40 +3,80 @@
 
 # CITATION:
 # My Python socket project from the beginning of this term
+#
+# Use of two ports, one for send one for receive, from instructor's
+# reply in Ed discussion board:
+# https://edstem.org/us/courses/29340/discussion/2175842
 from socket import *
+import threading
+import logging
+import time
+import concurrent.futures
 
 server_host = "localhost"
-server_port = 3291
+server_send_port = 3291
+server_receive_port = 8000
 initial_msg = "Waiting for messages..."
 quit_msg = "Type /q to quit."
 msg_num = 0
+
+
+def client_thread_func(conn, event):
+    while not event.is_set():
+        # Receive data from the client, with bufsize specified
+        client_data = conn.recv(1024).decode()
+        while len(client_data) > 0:
+            print(client_data)
+            client_data = conn.recv(1024).decode()
+
+        event.set()
+
+        # data_remaining = client_data.split(' ')[0]
+        # print(client_data[len(data_remaining)+1:])
+        # data_remaining -= len(client_data[len(data_remaining)+1:])
+        # while data_remaining > 0:
+        #     client_data = conn.recv(1024).decode()
+        #     print(client_data)
+        #     data_remaining -= len(client_data)
+
+        # print(f"CLIENT DATA: {client_data}")
+        # if len(client_data) > 0:
+        #     print(f"CLIENT MESSAGE LENGTH: {len(client_data)}")
+        #     msg_num += 1
+        #     if msg_num == 1:
+        #         print(quit_msg)
+        #     print(client_data.decode())
+        # else:
+        #     break
+
+        # print("No client data")
+
+
+def server_thread_func(conn, event):
+    while not event.is_set():
+        server_reply = input('> ')
+        server_reply = server_reply + '\n'
+        if server_reply == '/q':
+            print(f"Closing connection")
+            event.set()
+            conn.close()
+            return
+        conn.send(server_reply.encode())
+
 
 with socket(AF_INET, SOCK_STREAM) as sock:
     # Use of setsockopt taken from the project instructions
     # to help avoid "hanging" a port during testing
     sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    sock.bind((server_host, server_port))
+    sock.bind((server_host, server_send_port))
     sock.listen(1)  # Does not connect to multiple clients
+    print(f"Server listening on: {server_host} on port: {server_receive_port}")
     conn, addr = sock.accept()
     with conn:
-        print(f"Connected by ({conn}, {addr})\n"
+        print(f"Connected by ({addr})\n"
               f"{initial_msg}")
-        while True:
-            # Receive data from the client, with bufsize specified
-            client_data = conn.recv(1024)
-            if len(client_data) > 0:
-                msg_num += 1
-                if msg_num == 1:
-                    print(quit_msg)
-                print(client_data)
-            else:
-                break
+        event = threading.Event()
+        with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+            executor.submit(client_thread_func, conn, event)
+            executor.submit(server_thread_func, conn, event)
 
-        server_reply = input('> ')
-        if server_reply == "/q":
-            sock.close()
-        conn.send(server_reply.encode())
-
-
-if __name__ == '__main__':
-    print(f"Server listening on: {server_host} on port: {server_port}")
