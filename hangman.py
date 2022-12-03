98fb4d4baaca39be35226bf859e2caa3aea91b55
julialoy <julialoy@gmail.com>
2022-12-03 17:51:30 -0500

    Trying to fix some odd line breaks and shutting down properly when trying to quit immediately after hangman game is finished.

--- hangman.py
+++ hangman.py
@@ -1,8 +1,11 @@
 import random
+
+
 # CITATION:
 # WikiHow's Hangman article was consulted for the order of drawing
 # the hanged man when a player answers incorrectly and for basic
 # gameplay instructions:
+# WikiHow. (Nov. 30, 2022). "Play Hangman."
 # https://www.wikihow.com/Play-Hangman
 class HangmanGame:
     """
@@ -14,6 +17,8 @@ class HangmanGame:
     that player wins. If neither player guesses correctly before the drawing is
     complete, both players lose.
     """
+    # Strings showing the gradual progression of drawing the Hanged Man
+    # when players guess incorrectly
     _first_wrong = "  ____\n" + "      |\n" + "      |\n" + "      |\n" + "      |\n"
     _second_wrong = "  ____\n" + "      |\n" + " 0    |\n" + "      |\n" + "      |\n"
     _third_wrong = "  ____\n" + "      |\n" + " 0    |\n" + " |    |\n" + "      |\n"
@@ -26,6 +31,7 @@ class HangmanGame:
         _first_wrong, _second_wrong, _third_wrong, _fourth_wrong,
         _fifth_wrong, _sixth_wrong, _seventh_wrong, _eighth_wrong
     ]
+    # Word bank, game instructions, and game over message
     _word_bank = [
         "banana",
         "computer",
@@ -37,62 +43,88 @@ class HangmanGame:
         "swimming",
         "blanket"
     ]
-    instructions = "******************* HANGMAN *******************************\n" \
-                   "A random word will be selected by the game. The length" \
-                    " will be indicated by blanks. You will take turns guessing," \
-                    " only guessing when prompted. If you and your partner correctly " \
-                    "guess the secret word before the drawing of the Hanged Man is complete," \
-                    " you both win! But if you don't guess correctly before the drawing is complete" \
-                    " you both lose. Good luck!\n" \
-                    "**********************************************************\n"
+    _instructions = ("******************* HANGMAN *******************************\n"
+                    "A random word will be selected by the game. The length \n"
+                    "will be indicated by blanks. You will take turns guessing, \n"
+                    "only guessing when prompted. If you and your partner correctly \n"
+                    "guess the secret word before the drawing of the Hanged Man is complete, \n"
+                    "you both win! But if you don't guess correctly before the drawing is complete \n"
+                    "you both lose. Good luck!\n" \
+                    "**********************************************************\n")
+    _game_over = ("******************* GAME OVER *******************************")
 
-    def __init__(self, conn=None):
+    def __init__(self, conn, player_one, player_two):
         self._secret_word = self._word_bank[random.randint(0, len(self._word_bank)-1)]
         self._is_game_over = False
         self._guessed_letters = []
-        self._progress = ["_" for _ in self._secret_word]   # _ used since variable not needed
+        self._progress = ["_" for _ in self._secret_word]
         self._num_wrong_guesses = 0
-        self._player_turn = 1
+        self._player_one = player_one
+        self._player_two = player_two
+        self._player_turn = 1       # The player initiating the game is always player 1
         self._connection = conn
 
     def get_game_over(self):
+        """Returns game state."""
         return self._is_game_over
 
     def get_letters_guessed(self):
+        """Returns the letters already guessed as a space-separated string."""
         if self._guessed_letters:
             return f"Letters guessed: {' '.join(self._guessed_letters)}"
 
-    def get_player_guess(self):
-        player_guess = input('Guess a letter: ')
+    def get_player_guess(self, player):
+        """Retrieves a player's guess, ensures the guess is only 1 letter
+        and is not a duplicate guess, adds the guess to the _guessed_letters
+        list and returns the guess.
+        """
+        if player == self._player_one:
+            player_guess = input("Guess a letter: \n> ")
+        else:
+            self.send_hangman("Guess a letter: \n")
+            self.send_hangman('/f')
+            player_guess = self.recv_hangman()
 
         while player_guess.lower() in self._guessed_letters:
-            player_guess = input(
-                'You already guessed that letter.\nGuess a letter: ')
+            if player == self._player_one:
+                player_guess = input(
+                    "You already guessed that letter.\nGuess a letter: ")
+            else:
+                self.send_hangman("You already guessed that letter.\nGuess a letter: \n")
+                self.send_hangman('/f')
+                player_guess = self.recv_hangman()
 
-        while len(player_guess) > 1:
-            player_guess = input(
-                'You may only guess one letter.\nGuess a letter: ')
+        # while len(player_guess) > 1:
+        #     player_guess = input(
+        #         'You may only guess one letter.\nGuess a letter: ')
 
         self._guessed_letters.append(player_guess.lower())
 
         return player_guess
 
     def get_game_progress(self):
+        """Returns the current state of the word to guess, with letters filled
+        in if they have been guessed and unguessed letters as blanks.
+        """
         return f"Word to guess: {' '.join(self._progress)}"
 
     def get_guess_result(self, guess=None):
+        """Evaluates the player's guess to determine whether it was a
+        correct or incorrect guess, then updates the game progress and
+        game state.
+        """
         player_guess = self._guessed_letters[-1] if not guess else guess
         if player_guess not in self._secret_word:
             self._num_wrong_guesses += 1
             # print(self._wrong_guesses[self._num_wrong_guesses - 1])
             if self._num_wrong_guesses < 8:
-                return f"{self._wrong_guesses[self._num_wrong_guesses - 1]}\n" \
-                       f"Sorry! That letter is not in the word."
+                return (f"{self._wrong_guesses[self._num_wrong_guesses - 1]}\n"
+                        f"Sorry! That letter is not in the word.")
             else:
                 self._is_game_over = True
-                return f"{self._wrong_guesses[self._num_wrong_guesses - 1]}\n" \
-                      f"The word was {self._secret_word}.\n" \
-                       f"Game Over! You lost!"
+                return (f"{self._wrong_guesses[self._num_wrong_guesses - 1]}\n"
+                        f"The word was {self._secret_word}.\n"
+                        f"Game Over! You lost!")
         else:
             for i in range(len(self._secret_word)):
                 if self._secret_word[i] == player_guess.lower():
@@ -104,57 +136,111 @@ class HangmanGame:
             return "That is a correct guess!"
 
     def get_player_turn(self):
+        """Returns the number of the player who is currently allowed
+        to guess.
+        """
         return self._player_turn
 
     def advance_turn(self):
+        """Advances the gameplay to the next player."""
         self._player_turn = 2 if self._player_turn == 1 else 1
 
-    def send_hangman_info(self, info):
-        self._connection.send(info.encode())
+    def send_hangman(self, data):
+        data = data
+        self._connection.sendall(data.encode())
 
-    def get_hangman_info(self):
-        self._connection.recv(1024)
+    def recv_hangman(self):
+        guess = self._connection.recv(1).decode()
+        return guess
 
-    def play_game(self):
-        while not self._is_game_over:
-            current_progress = f"Word to guess: {' '.join(self._progress)}"
-            print(current_progress)
-            self.send_hangman_info(current_progress)
+    def play_hangman(self):
+        # if player_number == 1:
+        #     new_game = HangmanGame()
+        #     self._hangman_game = new_game
 
-            if self._guessed_letters:
-                current_guessed = f"Letters guessed: {' '.join(self._guessed_letters)}"
-                print(current_guessed)
-                self.send_hangman_info(current_guessed)
+        print(self._instructions)
+        self.send_hangman('\n'+self._instructions+'\n')
+        while not self.get_game_over():
+            game_progress = self.get_game_progress()
+            print(game_progress)
+            self.send_hangman(game_progress+'\n')
 
-            self.send_hangman_info("OTHER PLAYER IS GUESSING")
-            player_guess = input('Guess a letter: ')
+            guessed_letters = self.get_letters_guessed()
+            if guessed_letters:
+                print(guessed_letters)
+                self.send_hangman(guessed_letters+'\n')
 
-            while player_guess.lower() in self._guessed_letters:
-                player_guess = input('You already guessed that letter.\nGuess a letter: ')
-
-            while len(player_guess) > 1:
-                player_guess = input('You may only guess one letter.\nGuess a letter: ')
-
-            self._guessed_letters.append(player_guess.lower())
-            self.send_hangman_info(f"OTHER PLAYER GUESSED {player_guess.lower()}")
-
-            if player_guess.lower() not in self._secret_word:
-                self._num_wrong_guesses += 1
-                print(self._wrong_guesses[self._num_wrong_guesses-1])
-                if self._num_wrong_guesses < 8:
-                    response = 'Sorry! That letter is not in the word.'
-                else:
-                    print(f"The word was {self._secret_word}.")
-                    print('Game Over! You lost!')
-                    self._is_game_over = True
+            if self.get_player_turn() == 1:
+                self.send_hangman(f"{self._player_one} IS GUESSING")
+                player_one_guess = self.get_player_guess(self._player_one)
+                self.send_hangman(f"{self._player_one} GUESSED {player_one_guess}")
+                game_response = self.get_guess_result(player_one_guess)
+                print(game_response)
+                self.send_hangman(game_response)
             else:
-                print('That is a correct guess!')
-                for i in range(len(self._secret_word)):
-                    if self._secret_word[i] == player_guess.lower():
-                        self._progress[i] = self._secret_word[i]
+                print(f"{self._player_two} IS GUESSING")
+                player_two_guess = self.get_player_guess(self._player_two)
+                print(f"{self._player_two} GUESSED {player_two_guess}")
+                game_response = self.get_guess_result(player_two_guess)
+                print(game_response)
+                self.send_hangman(game_response)
 
-            if "_" not in self._progress:
-                self._is_game_over = True
-                print(f"The word was {self._secret_word}.")
-                print("You won!")
+            self.advance_turn()
+
+        # self._hangman_game = None
+        # self._hangman_started = False
+        print(self._game_over)
+        self.send_hangman(self._game_over)
+        self.send_hangman('/endhangman')
+        self.send_hangman('/f')
+
+    #
+    # def send_hangman_info(self, info):
+    #     self._connection.send(info.encode())
+    #
+    # def get_hangman_info(self):
+    #     self._connection.recv(1024)
+    #
+    # def play_game(self):
+    #     while not self._is_game_over:
+    #         current_progress = f"Word to guess: {' '.join(self._progress)}"
+    #         print(current_progress)
+    #         self.send_hangman_info(current_progress)
+    #
+    #         if self._guessed_letters:
+    #             current_guessed = f"Letters guessed: {' '.join(self._guessed_letters)}"
+    #             print(current_guessed)
+    #             self.send_hangman_info(current_guessed)
+    #
+    #         self.send_hangman_info("OTHER PLAYER IS GUESSING")
+    #         player_guess = input('Guess a letter: ')
+    #
+    #         while player_guess.lower() in self._guessed_letters:
+    #             player_guess = input('You already guessed that letter.\nGuess a letter: ')
+    #
+    #         while len(player_guess) > 1:
+    #             player_guess = input('You may only guess one letter.\nGuess a letter: ')
+    #
+    #         self._guessed_letters.append(player_guess.lower())
+    #         self.send_hangman_info(f"OTHER PLAYER GUESSED {player_guess.lower()}")
+    #
+    #         if player_guess.lower() not in self._secret_word:
+    #             self._num_wrong_guesses += 1
+    #             print(self._wrong_guesses[self._num_wrong_guesses-1])
+    #             if self._num_wrong_guesses < 8:
+    #                 response = 'Sorry! That letter is not in the word.'
+    #             else:
+    #                 print(f"The word was {self._secret_word}.")
+    #                 print('Game Over! You lost!')
+    #                 self._is_game_over = True
+    #         else:
+    #             print('That is a correct guess!')
+    #             for i in range(len(self._secret_word)):
+    #                 if self._secret_word[i] == player_guess.lower():
+    #                     self._progress[i] = self._secret_word[i]
+    #
+    #         if "_" not in self._progress:
+    #             self._is_game_over = True
+    #             print(f"The word was {self._secret_word}.")
+    #             print("You won!")
 
--- server_simple.py
+++ server_simple.py
@@ -1,77 +1,83 @@
-# CITATION:
-# Python docs. How to: Socket programming
-
-# CITATION:
-# My Python socket project from the beginning of this term
-#
-# Use of two ports, one for send one for receive, from instructor's
-# reply in Ed discussion board:
-# https://edstem.org/us/courses/29340/discussion/2175842
 from socket import *
 
 from hangman import HangmanGame
 
 
+# CITATION:
+# Use of class for server was inspired by and adapted from
+# McMillan, G. (Nov. 30, 2022). MySocket code example in "Socket Programming
+# HOWTO" in Python documentation. [Python docs v. 3.4.10]
+# https://docs.python.org/3.4/howto/sockets.html
 class Server:
-
+    """Server object creates a server chat and tracks the various states, including
+    connection, whether a message has been received from a client, and
+    whether the Server and Client are playing a game of Hangman.
+    """
     def __init__(self):
-        self._host = "localhost"
+        self._host = 'localhost'
         self._port = 3291
         self._initial_msg = "Waiting for messages..."
-        self._instrux = "Type a message.\nType /f when finished sending messages.\nType /hangman to start a Hangman game.\nType /q to quit."
+        self._instrux = ("Type a message.\n"
+                         "  * Type /f after sending the messages you wish to send.\n"
+                         "  * Type /hangman to start a Hangman game.\n"
+                         "  * Type /q to quit.")
         self._is_first_reply = True
         self._hangman_started = False
         self._is_connected = False
         self._hangman_game = None
 
     def _get_client_data(self, conn):
+        """Handles retrieving any messages from the client."""
         if not self._is_connected:
             return
-
-        client_data = conn.recv(1024).decode()
-        while client_data[-2:] != '/f':
+        # client_data = conn.recv(1024).decode()
+        # CITATION:
+        # Use of delimiter to indicate when client has finished sending
+        # messages is based on information in
+        # McMillan, G. (Nov. 30, 2022). "Socket Programming
+        # HOWTO" in Python documentation. [Python docs v. 3.4.10]
+        # https://docs.python.org/3.4/howto/sockets.html
+        # while client_data[-2:] != '/f':
+        while True:
+            client_data = conn.recv(1024).decode()
             if '/hangman' in client_data:
                 self._hangman_started = True
 
             if client_data[-2:] == '/f' and len(client_data) > 2 and not self._hangman_started:
                 print(f"CLIENT: {client_data[:-2]}")
+                break
             elif client_data[-2:] == '/f' and len(client_data):
                 print(client_data[:-2])
+                if '/endhangman' in client_data:
+                    self._hangman_started = False
+                break
             elif client_data[-2:] == '/q':
                 self._is_connected = False
                 print("Client left the connection. Closing connection.")
                 break
-
-            if '/endhangman' in client_data:
-                self._hangman_started = False
-
-            if not self._hangman_started:
+            elif len(client_data) > 0 and not self._hangman_started:
                 print(f"CLIENT: {client_data}")
             elif len(client_data) > 0 and client_data != '/hangman':
+                if '/endhangman' in client_data:
+                    self._hangman_started = False
                 print(client_data)
 
-            client_data = conn.recv(1024).decode()
-            # if client_data[-2:] == '/f' and len(client_data) > 2 and not self._hangman_started:
-            #     print(f"CLIENT: {client_data[:-2]}")
-            # elif client_data[-2:] == '/f' and len(client_data):
-            #     print(client_data[:-2])
-            # elif client_data[-2:] == '/q':
-            #     self._is_connected = False
-            #
-            # if '/endhangman' in client_data:
-            #     self._hangman_started = False
-
     def _send_data(self, conn):
+        """Handles sending messages to the client."""
         if not self._is_connected:
             return
 
+        # Hangman game is turn based, server will only enter one input
         if self._hangman_started:
-            server_reply = input(' ')
-            conn.send(server_reply.encode())
-
-        while not self._hangman_started:
             server_reply = input('> ')
-            conn.send(server_reply.encode())
+            conn.sendall(server_reply.encode())
+
+        # When not playing Hangman, server can send as many messages
+        # as desired
+        while not self._hangman_started:
+             # print(f"NO HANGMAN GAME. SEND A MESSAGE.")
+            server_reply = input('> ')
+            conn.sendall(server_reply.encode())
             if server_reply == '/q':
                 print(f"Closing connection")
                 self._is_connected = False
@@ -80,66 +86,93 @@ class Server:
                 break
             elif server_reply == '/hangman':
                 self._hangman_started = True
+                # self.play_hangman(conn)
                 self.play_hangman(conn)
                 break
 
-    def send_client_hangman(self, conn, data):
-        data = data
-        conn.send(data.encode())
-
-    def recv_client_hangman(self, conn):
-        client_guess = conn.recv(1).decode()
-        return client_guess
-
-    def play_hangman(self, conn, player_number=1):
-        if player_number == 1:
-            new_game = HangmanGame()
-            self._hangman_game = new_game
-
-        print(self._hangman_game.instructions)
-        self.send_client_hangman(conn, self._hangman_game.instructions)
-        while not self._hangman_game.get_game_over():
-            game_progress = self._hangman_game.get_game_progress()
-            print(game_progress)
-            self.send_client_hangman(conn, game_progress)
-
-            guessed_letters = self._hangman_game.get_letters_guessed()
-            if guessed_letters:
-                print(guessed_letters)
-                self.send_client_hangman(conn, guessed_letters)
-
-            if self._hangman_game.get_player_turn() == 1:
-                self.send_client_hangman(conn, "SERVER IS GUESSING")
-                server_guess = self._hangman_game.get_player_guess()
-                self.send_client_hangman(conn, f"SERVER GUESSED {server_guess}")
-                game_response = self._hangman_game.get_guess_result(server_guess)
-                print(game_response)
-                self.send_client_hangman(conn, game_response+'\n')
-            else:
-                print("CLIENT IS GUESSING")
-                self.send_client_hangman(conn, "Guess a letter: ")
-                self.send_client_hangman(conn, '/f')
-                client_guess = self.recv_client_hangman(conn)
-                print(f"CLIENT GUESSED {client_guess}")
-                game_response = self._hangman_game.get_guess_result(client_guess)
-                print(game_response)
-                self.send_client_hangman(conn, game_response+'\n')
-
-            self._hangman_game.advance_turn()
-
+    def play_hangman(self, conn):
+        new_game = HangmanGame(conn, "SERVER", "CLIENT")
+        new_game.play_hangman()
         self._hangman_game = None
         self._hangman_started = False
-        "******************* GAME OVER *******************************"
-        self.send_client_hangman(conn, '/endhangman')
-        self.send_client_hangman(conn, '/f')
+    #
+    # def send_client_hangman(self, conn, data):
+    #     data = data
+    #     conn.send(data.encode())
+    #
+    # def recv_client_hangman(self, conn):
+    #     client_guess = conn.recv(1).decode()
+    #     return client_guess
+    #
+    # def play_hangman(self, conn, player_number=1):
+    #     if player_number == 1:
+    #         new_game = HangmanGame()
+    #         self._hangman_game = new_game
+    #
+    #     print(self._hangman_game.instructions)
+    #     self.send_client_hangman(conn, self._hangman_game.instructions)
+    #     while not self._hangman_game.get_game_over():
+    #         game_progress = self._hangman_game.get_game_progress()
+    #         print(game_progress)
+    #         self.send_client_hangman(conn, game_progress)
+    #
+    #         guessed_letters = self._hangman_game.get_letters_guessed()
+    #         if guessed_letters:
+    #             print(guessed_letters)
+    #             self.send_client_hangman(conn, guessed_letters)
+    #
+    #         if self._hangman_game.get_player_turn() == 1:
+    #             self.send_client_hangman(conn, "SERVER IS GUESSING")
+    #             server_guess = self._hangman_game.get_player_guess()
+    #             self.send_client_hangman(conn, f"SERVER GUESSED {server_guess}")
+    #             game_response = self._hangman_game.get_guess_result(server_guess)
+    #             print(game_response)
+    #             self.send_client_hangman(conn, game_response+'\n')
+    #         else:
+    #             print("CLIENT IS GUESSING")
+    #             self.send_client_hangman(conn, "Guess a letter: ")
+    #             self.send_client_hangman(conn, '/f')
+    #             client_guess = self.recv_client_hangman(conn)
+    #             print(f"CLIENT GUESSED {client_guess}")
+    #             game_response = self._hangman_game.get_guess_result(client_guess)
+    #             print(game_response)
+    #             self.send_client_hangman(conn, game_response+'\n')
+    #
+    #         self._hangman_game.advance_turn()
+    #
+    #     self._hangman_game = None
+    #     self._hangman_started = False
+    #     "******************* GAME OVER *******************************"
+    #     self.send_client_hangman(conn, '/endhangman')
+    #     self.send_client_hangman(conn, '/f')
 
     def server_run(self):
+        # CITATION:
+        # Creation of the socket adapted from my Sockets and HTTP programming
+        # project for this course this term:
+        # Loy, J. Sockets and HTTP programming project for OSU CS372, Fall
+        # 2022. [Source code.]
+        #
+        # The original citations from my Sockets and HTTP project are still
+        # relevant and are listed below:
+        #
+        # This code has also been adapted from the Python socket documentation,
+        # specifically the server socket example in the documentation, and from
+        # the example from the course textbook:
+        #
+        # Kurose, J., and Ross, K. Chapter 2, Section 2.7.2, "Socket
+        # Programming with TCP," in Computer Networking, 8e. [Code examples]
+        # Hoboken, NJ: Pearson, 2021.
+        #
+        # Python documentation. (Oct. 13, 2022) "Socket - Low-Level
+        # Networking Interface." (Python v. 3.10.8).
+        # https://docs.python.org/3/library/socket.html
         with socket(AF_INET, SOCK_STREAM) as sock:
             # Use of setsockopt taken from the project instructions
             # to help avoid "hanging" a port during testing
             sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
             sock.bind((self._host, self._port))
-            sock.listen(1)  # Does not connect to multiple clients
+            sock.listen(1)  # Does not handle multiple connections
             print(f"Server listening on: {self._host} on port: {self._port}")
             conn, addr = sock.accept()
             self._is_connected = True
@@ -148,13 +181,20 @@ class Server:
                       f"{self._initial_msg}")
                 while self._is_connected:
                     self._get_client_data(conn)
+
+                    # Server shuts down if client leaves connection
                     if not self._is_connected:
                         conn.close()
                         break
+
+                    # Print some instructions for the server once the
+                    # client sends the first message.
                     if self._is_first_reply and not self._hangman_started:
                         print(self._instrux)
                         self._is_first_reply = False
+
                     self._send_data(conn)
+                # conn.close()
 
 
 if __name__ == '__main__':
